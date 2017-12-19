c094198d057d16c4f56501e8650ba7a49f51f3bf
gganes <gganes@GIVSARDIN38951.sapient.com>
2017-12-19 18:11:45 +0530

    Final commit in 19th dec 2017. Continue Tommorrow

--- InterfaceDemo2/src/com/sapient/service/EmpFileDao.java
+++ InterfaceDemo2/src/com/sapient/service/EmpFileDao.java
@@ -1,16 +1,31 @@
 package com.sapient.service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmpFileDao implements Idao {
+	
+	EmpFileDao() {
+	}
 
 	@Override
 	public List<String> viewEmployee() {
 		System.out.println("This is EmpFileDao Instance");
 		List<String> lst = new ArrayList<String>();
-		lst.add("ram");
-		lst.add("prem");
+		String str = null;
+		try{
+			FileReader fr = new FileReader("emp.txt");
+			BufferedReader br =new BufferedReader(fr);
+			while((str = br.readLine()) != null){
+				lst.add(str);
+			}
+			br.close();
+			fr.close();
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		return lst;
 	}
 
