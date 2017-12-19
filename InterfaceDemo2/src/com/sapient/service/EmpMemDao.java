f6678f7920c9bd84ee5cd60b4b980b489bc88604
gganes <gganes@GIVSARDIN38951.sapient.com>
2017-12-19 17:36:56 +0530

    Created InterfaceDemo2

---
+++ InterfaceDemo2/src/com/sapient/service/EmpMemDao.java
@@ -0,0 +1,19 @@
+package com.sapient.service;
+
+import java.util.*;
+import java.util.List;
+
+public class EmpMemDao implements Idao {
+
+	EmpMemDao() {
+		// Instance cannot be created in another package since access is package private
+	}
+	@Override
+	public List<String> viewEmployee() {
+		List<String> lst = new ArrayList<String>();
+		lst.add("ram");
+		lst.add("sam");
+		return lst;
+	}
+
+}
