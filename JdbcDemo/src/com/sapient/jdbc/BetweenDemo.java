b8a1883b9f536f36dd78dc5075b4eff99b9cd106
gganes <gganes@GIVSARDIN38951.sapient.com>
2017-12-20 14:42:38 +0530

    Added BetweenDemo in jdbcDemo project

--- JdbcDemo/src/com/sapient/jdbc/BetweenDemo.java
+++ JdbcDemo/src/com/sapient/jdbc/BetweenDemo.java
@@ -5,16 +5,26 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
-public class ViewAll {
+public class BetweenDemo {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		Connection conn = DriverManager.getConnection(url,"system","Sapient123");
 		
-		String sql = "SELECT * FROM sap_employee";
+		String sql = "SELECT * FROM sap_employee WHERE sal  BETWEEN ? AND ?";
 		PreparedStatement st = conn.prepareStatement(sql);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Lowest Range :");
+		double min = sc.nextDouble();
+		System.out.print("Enter the Maximum Range :");
+		double max = sc.nextDouble();
+		
+		st.setDouble(1, min);
+		st.setDouble(2, max);
 		ResultSet rs = st.executeQuery();
 		
 		while(rs.next()){
@@ -24,13 +34,9 @@ public class ViewAll {
 			System.out.printf("%2d",rs.getInt("dept_id"));
 			System.out.printf("%12s", rs.getDate("doj"));
 			System.out.println();			
-//			System.out.print(rs.getInt("eid") + "\t");
-//			System.out.print(rs.getString("ename") + "\t");
-//			System.out.print(rs.getDouble("sal") + "\t");
-//			System.out.print(rs.getInt("dept_id") + "\t");
-//			System.out.println(rs.getDate("doj"));
 		}		
-		conn.close();				
+		conn.close();
+
 	}
 
 }
