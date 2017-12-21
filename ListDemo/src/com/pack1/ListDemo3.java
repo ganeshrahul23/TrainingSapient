5fad81acca6c1e91f6cad71ed8859450f7ab1711
gganes <gganes@GIVSARDIN38951.sapient.com>
2017-12-21 12:32:40 +0530

    Edited

---
+++ ListDemo/src/com/pack1/ListDemo3.java
@@ -0,0 +1,13 @@
+package com.pack1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDemo3 {
+
+	public static void main(String[] args) {
+		Integer [] arr = {2, 45, 67, 89, 01 ,23 };
+		List<Integer> lst = (List<Integer>)Arrays.asList(arr);
+	}
+
+}
