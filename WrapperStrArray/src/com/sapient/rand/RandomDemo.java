eac4739ca9668f456207d8a17bcadff02e607edf
gganes <gganes@GIVSARDIN38951.sapient.com>
2017-12-22 11:48:23 +0530

    Added RandomDemo

---
+++ WrapperStrArray/src/com/sapient/rand/RandomDemo.java
@@ -0,0 +1,22 @@
+package com.sapient.rand;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class RandomDemo {
+
+	public static void main(String[] args) {
+		System.out.println(gen5NOs());
+
+	}
+
+	public static Set<Integer> gen5NOs(){
+		Set<Integer> set= new HashSet<>();
+		Random ran = new Random();
+		while(set.size() < 5){
+			set.add(ran.nextInt(15)  + 1);
+		}		
+		return set;
+	}
+}
